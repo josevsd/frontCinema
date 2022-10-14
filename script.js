e78b4c17106d0fff814181886684584a83f36c20
unknown <jose.victor9936@gmail.com>
2022-10-14 11:09:33 -0300

    modal feito

---
+++ script.js
@@ -0,0 +1,11 @@
+const acao = () =>{
+    let modal = document.querySelector('.modal');
+
+    modal.style.display = 'block';
+}
+
+const fechar = () =>{
+    let modal = document.querySelector('.modal');
+
+    modal.style.display = 'none';
+}
